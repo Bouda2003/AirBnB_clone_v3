6b04de4e70a30290cde3c0964234a561dc7b4ecb
Abdelrahman Mohsen <aute.mohsen@gmail.com>
2024-08-05 18:05:07 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, Blueprint, make_response, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,6 +13,15 @@ app.register_blueprint(app_views)
 def close_db(error):
     """ Close Storage """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 
+    responses 404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
 
 
 if __name__ == "__main__":
